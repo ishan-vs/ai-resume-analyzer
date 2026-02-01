fa7ea000fc3efd9c1fbcdce4aeba10909d793000
ishan-vs <ishanking992005@gmail.com>
2026-02-01 10:53:42 +0530

    Cloud storage, PDF-Image conversion,Key-Value storage and AI feedback

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
-import Navbar from "../../components/Navbar";
+import Navbar from "~/components/Navbar";
 import {resumes} from " ../../constants";
-import ResumeCard from "../../components/ResumeCard";
+import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
 import {useLocation, useNavigate} from "react-router";
 import {useEffect} from "react";
